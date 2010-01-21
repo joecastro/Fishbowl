f2a7d8c220fe5bc3a70eb16ee037601afe82e803
JoeCastro_cp <SND\JoeCastro_cp>
2010-01-21 12:09:05 -0800

    Checking in on behalf of Eric.  This is most of the new dark theme.  This isn't quite complete and has issues with transitions that we're going to address in future checkins.

---
+++ FacebookClient/Fishbowl/Converters/EmptyStringToBooleanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace FacebookClient
+{
+    public class EmptyStringToBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            String s = value as String;
+            if (value != null && String.Empty.Equals(value)) return false;
+            else return true;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+
+        #endregion
+    }
+}
